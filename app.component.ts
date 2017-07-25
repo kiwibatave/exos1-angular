398e08dc5559194776b8eb287efef9e69a41f146
kiwibatave <b_cuzon@yahoo.fr>
2017-07-25 17:01:33 +0200

    second commit

--- app.component.ts
+++ app.component.ts
@@ -6,7 +6,33 @@ import {Component} from '@angular/core';
   <h2>Mon footballeur préféré est : {{myFootballeur}}</h2>
   <button (click)=afficherDate()>{{button}}</button>
   <p>{{ date }}</p>
-  <button (click)=afficheAlert()>{{bouton}}</button>`
+  <button (click)=afficheAlert()>{{bouton}}</button>
+  <h1> La liste des langages que je connais </h1>
+  <ul>
+  <li *ngFor="let langage of langages">
+   {{langage}}
+   </li>
+   </ul>
+   <h1>Exo 5</h1>
+   <p>
+   <img [src]="imagePath">
+   <button (click)=animatePhoto()>{{animation}}</button>
+   </p>
+   <h1>Exo 6</h1>
+   <p>
+   </p>
+   <h1>Exo 7</h1>
+   <p>
+   <ul>
+   <li *ngFor="let produit of produits">
+   {{produit}}
+   </li>
+   </ul>
+   <input #ingredientsAdd type="text" placeholder="ajoute ton ingrédient ici">
+   <button (click)=addProduit(ingredientsAdd)>{{texte}}</button>
+
+
+   `
   ,
 })
 export class AppComponent {
@@ -15,7 +41,7 @@ export class AppComponent {
   myFootballeur:string = 'Francis Llacer';
   // exo 2
   button:string = 'Quelle est la date du jour ?';
-  date : Date;
+  date: Date;
 
   afficherDate () {
     this.date = new Date();
@@ -26,6 +52,15 @@ export class AppComponent {
   afficheAlert () {
     alert("Angular, c\'est de la  BOMBE !!!");
   }
+  // exo 4
+  langages:string [] = ['HTML', 'CSS', 'PHP', 'JAVACRIPT'];
+  // exo 5
+  imagePath:string = ('./image1.png');
+  animation:string ='Click pour animer le bonhomme !!!';
 
+  // exo 6
 
+  // exo 7
+  produits:string [] = ['pain', 'lait', 'beurre', 'farine'];
+  texte:string = 'Click pour ajouter ton ingrédient';
 }
